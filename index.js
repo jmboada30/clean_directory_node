b7f9fb801169e9a1b87658c0725b3c4b70d83938
Joel Boada <jmboada30@gmail.com>
2024-03-06 19:32:04 -0400

    Add TASK_EXECUTION_INTERVAL validation

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import * as schedule from 'node-schedule';
 import { deleteOldFiles, getCurrentDate } from './deleteOldFiles.js';
+import 'dotenv/config';
+
+const interval = Number(process.env.TASK_EXECUTION_INTERVAL);
+
+// Validar que la variable de entorno FILE_PATH esté definida
+if (!interval) {
+  console.error(
+    'La variable de entorno TASK_EXECUTION_INTERVAL no está definida'
+  );
+  process.exit(1);
+}
 
 // Crea la tarea programada pero no la inicia
 const job = new schedule.Job('deleteOldFiles', deleteOldFiles);
 
 // Configura la regla de programación
 const rule = new schedule.RecurrenceRule();
-rule.minute = new schedule.Range(0, 59, 1); // Ejecuta cada minuto
+rule.minute = new schedule.Range(0, 59, interval); // Ejecuta cada minuto
 
 // Programa la tarea con la regla
 job.schedule(rule);
